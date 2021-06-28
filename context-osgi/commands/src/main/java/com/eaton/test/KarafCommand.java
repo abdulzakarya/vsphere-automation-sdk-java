9946aecf11a64a9f229f5dce02b698c970c169ca
Aboud Zakarya <AbdulZakarya@eaton.com>
2021-06-28 13:52:51 +0200

    use samples as a dependency and execute tagging samples as a karaf command

--- context-osgi/commands/src/main/java/com/eaton/test/KarafCommand.java
+++ context-osgi/commands/src/main/java/com/eaton/test/KarafCommand.java
@@ -3,6 +3,7 @@ package com.eaton.test;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.apache.karaf.shell.api.action.Action;
+import vmware.samples.tagging.workflow.TaggingWorkflow;
 
 @Command(scope = "cmd", name = "execute", description = "execute a karaf command")
 @Service
@@ -10,7 +11,10 @@ public class KarafCommand implements Action {
 
     @Override
     public Object execute() throws Exception {
-        System.out.println("hello world!");
+
+        String[] args = new String[0];
+        TaggingWorkflow.main(args);
+
         return null;
     }
 
